dcfb221816b141603fefa5823604f2b9b4366e01
mottioh <47217133+mottioh@users.noreply.github.com>
2020-10-14 20:22:26 +0300

    Delete install_nginx.sh

--- install_nginx.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo yum -y install nginx
-sudo mv /usr/share/nginx/html/index.html index2.html
-echo "<h1> <strong>OpsSchool Rules</strong></h1>" | sudo tee /usr/share/nginx/html/index.html
-sudo service nginx start
